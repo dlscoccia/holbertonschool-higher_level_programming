1d709c239016c14a201d1d5f0f7e331fd1773d27
dlscoccia <2054@holbertonschool.com>
2020-09-03 17:48:49 +0000

    Task 1; v2 Fixing formatting

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from calculator_1 import add, sub, mul, div
 # You are not allowed to use * for importing or __import__
 
@@ -8,10 +7,10 @@ def main():
     # a and b must be defined in 2 different lines: a = 10 and another b = 5
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 # Your code should not be executed when imported
 if __name__ == "__main__":
     main()
