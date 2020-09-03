fe8501495aa09a0d272e163d25824509a58c01c7
dlscoccia <2054@holbertonschool.com>
2020-09-03 23:39:39 +0000

    Task 100 Fixing formatting; v2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 import sys
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = sys.argv[1]
     op = sys.argv[2]
-    b = sys.argv[3]
-
     if op != "+" and op != "-" and op != "*" and op != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     elif sys.argv[2] == "+":
-        print("{} {} {} = {}".format(a, op, b, add(int(a), int(b))))
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
     elif sys.argv[2] == "-":
-        print("{} {} {} = {}".format(a, op, b, sub(int(a), int(b))))
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
     elif sys.argv[2] == "*":
-        print("{} {} {} = {}".format(a, op, b, mul(int(a), int(b))))
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
     else:
-        print("{} {} {} = {}".format(a, op, b, div(int(a), int(b))))
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
+    print("{} {} {} = {}".format(sys.argv[1], op, sys.argv[3], result))
