4a75cb370aadda960a290dcfb0076a4dc6fda78d
dlscoccia <2054@holbertonschool.com>
2020-09-03 17:29:24 +0000

    Fixing function name

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def hola():
-    # a and b must be defined in 2 different lines: a = 1 and another b = 2
+
+def main():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
-# Your code should not be executed when imported - by using __import__
 if __name__ == "__main__":
-    hola()
+    main()
