8a23f37bc6cdcc6cded756ffea416f76a2dad4f8
dlscoccia <2054@holbertonschool.com>
2020-09-11 12:10:07 +0000

    Adding Task 12; v1

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (roman_string is None):
+        return (0)
+    I = 1
+    V = 5
+    X = 10
+    L = 50
+    C = 100
+    D = 500
+    M = 1000
+    number = 0
+    for i in roman_string:
+        if i == 'I':
+            number += 1
+        elif i == 'V':
+            number += 5
+        elif i == 'X':
+            number += 10
+        elif i == 'L':
+            number += 50
+        elif i == 'C':
+            number += 100
+        elif i == 'D':
+            number += 500
+        elif i == 'M':
+            number += 1000
+    return (number)
