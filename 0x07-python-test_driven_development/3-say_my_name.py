aab4eced99a924faf8b029d30715464d05f9e46f
dlscoccia <2054@holbertonschool.com>
2020-10-08 17:01:37 +0000

    Adding Task 3 with test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Module '''
+
+
+def say_my_name(first_name, last_name=""):
+    ''' say my name '''
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
