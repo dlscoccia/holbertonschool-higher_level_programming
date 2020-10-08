65c4c6cd3aff03fd1a5da133bf63ddc3c0c8a79a
dlscoccia <2054@holbertonschool.com>
2020-10-08 18:16:41 +0000

    Adding task 6 test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,35 +1,63 @@
-""" unit test for max_integer function """
-
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
-
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaximumInteger(unittest.TestCase):
-    """
-        unit testing class
-    """
-
-    def test_with_no_input(self):
-        self.assertEqual(max_integer(), None)
-
-    def test_valid_input(self):
-        lst = [1, 2, 3, 50]
-        self.assertEqual(max_integer(lst), 50)
-
-    def test_float(self):
-        lst = [1.2, 2.3, 3.4, 50.8]
-        self.assertEqual(max_integer(lst), 50.8)
-
-    def test_mixed_integers(self):
-        lst = [-1, -2, 3, -50]
-        self.assertEqual(max_integer(lst), 3)
-
-    def test_tuple_data(self):
-        lst = (-1, -2, 3, -50)
-        self.assertEqual(max_integer(lst), 3)
-
-    def test_non_valid_list(self):
-        new_list = ['hello', 0, 0, 0]
+class TestMaxInteger(unittest.TestCase):
+    def test_none_value(self):
+        '''
+            Testing when the paratemerter None is passed
+        '''
         with self.assertRaises(TypeError):
-            max_integer(new_list)
+            max_integer(None)
+
+    def test_right_output(self):
+        '''
+            Testing when passing and argument that is expected to succeed
+        '''
+        self.assertEqual(max_integer([1, 6, 100, 4, 0, -1, 10]), 100)
+
+    def test_empty_list(self):
+        '''
+            Testing an empty list parameter
+        '''
+        self.assertIsNone(max_integer([]))
+
+    def test_no_param(self):
+        '''
+            Testing no parameter
+        '''
+        self.assertIsNone(max_integer())
+
+    def test_equal_values(self):
+        '''
+            Testing with all the list values equal to each other
+        '''
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+
+    def test_no_ints(self):
+        '''
+            Testing with values in the list that are not integers
+        '''
+        with self.assertRaises(TypeError):
+            max_integer([1, "hello", 2, 3])
+
+    def test_only_negatives(self):
+        '''
+            Testing with negative numbers
+        '''
+        self.assertEqual(max_integer([-10, -100, -6, -3, -1]), -1)
+
+    def test_begining(self):
+        '''
+            Testing with all the list values equal to each other
+        '''
+        self.assertEqual(max_integer([100, 1, 1, 1]), 100)
+
+    def test_one(self):
+        '''
+            Testing with all the list values equal to each other
+        '''
+        self.assertEqual(max_integer([1]), 1)
