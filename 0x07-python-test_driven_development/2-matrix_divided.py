2f05b63a8066741904834476bc568858a040ee94
dlscoccia <2054@holbertonschool.com>
2020-10-08 16:30:26 +0000

    Pusshing Task 2 with test

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+def matrix_divided(matrix, div):
+    '''
+        divides a matrix value by a divisor
+    '''
+    new_matrix = []
+    length = len(matrix[0])
+    err = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        new_row = []
+        if not isinstance(row, list):
+            raise TypeError(err)
+        if length != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(err)
+            result = round(element / div, 2)
+            new_row.append(result)
+        new_matrix.append(new_row)
+    return new_matrix
