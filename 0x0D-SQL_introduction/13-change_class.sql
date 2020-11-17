9355c8bf540ca41497853ad4b61ab49071ffb7ad
dlscoccia <2054@holbertonschool.com>
2020-11-17 00:20:51 +0000

    Fixing IDK what

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
 -- Score too low
--- script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-DELETE FROM second_table
-WHERE score <= 5;
+-- script that removes all records with a score <= 5 in the table second_table.
+DELETE FROM second_table WHERE score <= 5;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,3 @@
 -- Average
--- script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score)
-FROM second_table;
+-- script that computes the score average of all records in the table second_table.
+SELECT AVG(score) FROM second_table;
