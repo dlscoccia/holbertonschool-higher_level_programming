27064a8d748a98965faa7498ac9bd6c539cfaaa2
dlscoccia <2054@holbertonschool.com>
2020-12-18 22:25:49 +0000

    Updating task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''find a peak module'''
+
+
+def find_peak(list_of_integers):
+    """ function that finds a peak in a list of integers """
+    if len(list_of_integers) > 0:
+        list_of_integers.sort()
+        return list_of_integers[-1]
