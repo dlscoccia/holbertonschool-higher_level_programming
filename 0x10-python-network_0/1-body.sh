e5189ab5a728eefab290f69efc64a471749c8190
dlscoccia <2054@holbertonschool.com>
2020-12-18 23:18:26 +0000

    Updating task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and follow the redirection
-curl -Ls "$1"
+# Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sI $1 | grep 'Location' |  cut -d '/' -f 4
