d585d9ba3589ef7277ba8ffe6b6cb0bfacb64716
dlscoccia <2054@holbertonschool.com>
2021-01-11 19:58:06 +0000

    Fixing output task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-# Python script to fetch an URL
-import urllib.request
-
-
-def fetcher():
-    """fetcher url function"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('utf8')))
-
+"""
+fetcher module
+"""
 if __name__ == "__main__":
-    fetcher()
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
