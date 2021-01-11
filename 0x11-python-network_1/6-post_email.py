bb91a55e71328e1d5a9b6fad496b17b0e76ac035
dlscoccia <2054@holbertonschool.com>
2021-01-11 21:58:22 +0000

    Adding task 6 with pep8

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+post email with requests module
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    req = requests.post(argv[1], data={'email': argv[2]})
+    print(req.text)
