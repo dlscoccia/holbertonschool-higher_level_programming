f27397fcf17c5761f7ac160fb7b1d3feb0c600c9
dlscoccia <2054@holbertonschool.com>
2021-01-11 23:45:25 +0000

    Adding task 8 with pep8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+json with api post resquets
+'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    response = requests.post(url, data={'q': q})
+    if response:
+        json_response = response.json()
+        id = json_response.get('id')
+        name = json_response.get('name')
+        if len(json_response) == 0 or not id or not name:
+            print('No')
+        else:
+            print('[{}] {}'.format(id, name))
+    else:
+        print('Not a valid JSON')
