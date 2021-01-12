a14488acd962f11d567a23fd2ef7bfcfe71390ab
dlscoccia <2054@holbertonschool.com>
2021-01-12 00:55:08 +0000

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,9 +7,9 @@ commits github api
 if __name__ == '__main__':
     import requests
     from sys import argv
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
-    commits = r.json()
+    commits = req.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
