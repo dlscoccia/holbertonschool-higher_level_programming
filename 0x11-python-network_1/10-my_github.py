487ec517ecd1332359415f6507f1d6949ba554f8
dlscoccia <2054@holbertonschool.com>
2021-01-12 00:34:12 +0000

    wo wo wo

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,9 +7,10 @@ github api resquest id from user
 if __name__ == "__main__":
     import requests
     from sys import argv
-    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    r = r.json()
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(argv[1], argv[2]))
+    req = req.json()
     try:
-        print(r['id'])
+        print(req['id'])
     except KeyError:
         print("None")
