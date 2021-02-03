68bac780c1cfeedd0b3efa87c0601f49d8c28bc1
dlscoccia <2054@holbertonschool.com>
2021-02-03 02:36:25 +0000

    Fixing output string on task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const mySize = process.argv[2];
 let i;
 if (isNaN(mySize)) {
-  console.log('Missing argument');
+  console.log('Missing size');
 } else {
   for (i = 0; i < mySize; i++) {
     console.log('X'.repeat(mySize));
