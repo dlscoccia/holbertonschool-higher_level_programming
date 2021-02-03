d80b9f10745457356b6b1573342a00b83b3d1dfb
dlscoccia <2054@holbertonschool.com>
2021-02-03 02:34:41 +0000

    Adding 2nd group of task 5..8

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const argvNumber = process.argv[2];
+let i;
+for (i = 0; i < argvNumber; i++) {
+  console.log('C is fun');
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const mySize = process.argv[2];
+let i;
+if (isNaN(mySize)) {
+  console.log('Missing argument');
+} else {
+  for (i = 0; i < mySize; i++) {
+    console.log('X'.repeat(mySize));
+  }
+}
