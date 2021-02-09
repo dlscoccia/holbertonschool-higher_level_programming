ffbd30a1436dbcb41a92f4e776b02a042975ab40
dlscoccia <2054@holbertonschool.com>
2021-02-09 01:37:04 +0000

    Adding task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  const val = dict[key];
+  if (!(dict[val] in newDict)) {
+    newDict[val] = [];
+  }
+}
+for (const key in dict) {
+  const val = dict[key];
+  newDict[val].push(key);
+}
+console.log(newDict);
