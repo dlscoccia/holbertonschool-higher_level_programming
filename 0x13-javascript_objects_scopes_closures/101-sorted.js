1e6c2a32ca85e19c5f53c0dd41c35c1c8c62a9b1
dlscoccia <2054@holbertonschool.com>
2021-02-09 10:59:25 -0500

    Fixing semistandard

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,11 +3,11 @@
 const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-    const val = dict[key];
-    newDict[val] = [];
+  const val = dict[key];
+  newDict[val] = [];
 }
 for (const key in dict) {
-    const val = dict[key];
-    newDict[val].push(key);
+  const val = dict[key];
+  newDict[val].push(key);
 }
 console.log(newDict);
