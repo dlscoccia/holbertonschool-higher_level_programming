58ff9228e3bc2ce59a57ccd15b59f9310e226723
dlscoccia <2054@holbertonschool.com>
2021-02-10 08:56:54 -0500

    jum

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+let fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+fs = require('fs');
+fs.writeFile(file, content, function (err) {
+  if (err) return console.log(err);
+  console.log(`${content} > ${file}`);
+});
