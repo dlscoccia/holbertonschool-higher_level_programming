91b081f10a62c5a16fb0a39df103ea7da4b3cd47
dlscoccia <2054@holbertonschool.com>
2021-02-10 11:23:05 -0500

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+//  script that prints the number of movies where the character “Wedge Antilles” is present.
+// ID of the character = 18
+let counter = 0;
+const id = '/18/';
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/'; // process.argv[2];
+request(url, function (error, response, body) {
+  if (error) console.error('error:', error);
+  const numberFilms = JSON.parse(body).count;
+  for (let i = 0; i < numberFilms; i++) {
+    const characters = JSON.parse(body).results[i].characters;
+    characters.forEach(char => {
+      // console.log(char);
+      if (char.includes(id)) {
+        counter += 1;
+      }
+    });
+  }
+  console.log(counter);
+});
