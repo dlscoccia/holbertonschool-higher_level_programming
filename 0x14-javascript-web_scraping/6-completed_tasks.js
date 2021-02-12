cb12cfd91e34c4743e74feda4583a9826fed64c0
dlscoccia <2054@holbertonschool.com>
2021-02-12 09:33:20 -0500

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+const completedDict = {};
+let counter = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const data = JSON.parse(body);
+  let lastUser = '';
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].userId !== lastUser) {
+      counter = 0;
+    }
+    if (data[i].completed === true) {
+      counter += 1;
+      completedDict[data[i].userId] = counter;
+    }
+    lastUser = data[i].userId;
+  }
+  console.log(completedDict);
+});
